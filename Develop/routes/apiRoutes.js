1b08e1d847df7f61535e53d5113f9a9d69fe3c4d
k1te <kmiller343@gmail.com>
2020-10-24 11:14:32 -0500

    new notes now pushed to db.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 module.exports = function (app) {
     app.post("/api/notes", function(req, res) {
-        fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
             if (error) throw error;
             const notes = JSON.parse(response);
             const noteAdded = req.body;
@@ -14,7 +14,8 @@ module.exports = function (app) {
                 text: noteAdded.text
             };
             notes.push(newNote);
-            fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(error) {
+            console.log(notes);
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
                 if (error) throw error;
             });
         });
