347525ae60519584f46b2a79a672d543d816097c
k1te <kmiller343@gmail.com>
2020-10-24 12:55:19 -0500

    rewrote apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,31 +1,49 @@
 var path = require("path");
 var fs = require("fs");
 
+
+
+
+
 module.exports = function (app) {
-    app.get("/api/notes", function(req, res) {
-        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
-            if (error) throw error;
-            let noteListItems = JSON.parse(response);
-            res.send(noteListItems);
-        })
-    })
-    app.post("/api/notes", function(req, res) {
-        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
-            if (error) throw error;
-            const notes = JSON.parse(response);
-            const noteAdded = req.body;
-            const noteID = notes.length + 1;
-            const newNote = {
-                id: noteID,
-                title: noteAdded.title,
-                text: noteAdded.text
-            };
-            notes.push(newNote);
-            console.log(notes);
-            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
-                if (error) throw error;
-            });
-        });
-    })
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, data) {
+        if (error) throw error;
+        let notes = JSON.parse(data);
     
-}
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    })
+    app.post("/api/notes", function (req, res) {
+      const noteAdded = req.body;
+      const noteID = notes.length + 1;
+      const newNote = {
+        id: noteID,
+        title: noteAdded.title,
+        text: noteAdded.text,
+      };
+      notes.push(newNote);
+      console.log(notes);
+      fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
+          if (error) throw error;
+      })
+    });
+    // app.post("/api/notes", function(req, res) {
+    //     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
+    //         if (error) throw error;
+    //         const notes = JSON.parse(response);
+    //         const noteAdded = req.body;
+    //         const noteID = notes.length + 1;
+    //         const newNote = {
+    //             id: noteID,
+    //             title: noteAdded.title,
+    //             text: noteAdded.text
+    //         };
+    //         notes.push(newNote);
+    //         console.log(notes);
+    //         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
+    //             if (error) throw error;
+    //         });
+    //     });
+    // })
+    
+})}
