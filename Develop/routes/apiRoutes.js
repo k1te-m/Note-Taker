9bbe8b350f4d4142b10e59b978a1be87212989ff
k1te <kmiller343@gmail.com>
2020-10-24 18:16:22 -0500

    without wildcard route, notes now dipslay

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,11 +7,12 @@ module.exports = function (app) {
     data
   ) {
     if (error) throw error;
-    let notes = JSON.parse(data);
-
-    app.get("/api/notes", function (req, res) {
+    const notes = JSON.parse(data);
+   
+    app.get("/api/notes", function(req, res) {
       res.json(notes);
-    });
+    })
+
     app.post("/api/notes", function (req, res) {
       const noteAdded = req.body;
       const noteID = notes.length + 1;
@@ -27,26 +28,23 @@ module.exports = function (app) {
         JSON.stringify(notes, null, 2),
         function (error) {
           if (error) throw error;
+          res.redirect("/notes");
+          console.log("Note created...");
         }
       );
     });
-    // app.post("/api/notes", function(req, res) {
-    //     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
-    //         if (error) throw error;
-    //         const notes = JSON.parse(response);
-    //         const noteAdded = req.body;
-    //         const noteID = notes.length + 1;
-    //         const newNote = {
-    //             id: noteID,
-    //             title: noteAdded.title,
-    //             text: noteAdded.text
-    //         };
-    //         notes.push(newNote);
-    //         console.log(notes);
-    //         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
-    //             if (error) throw error;
-    //         });
-    //     });
+
+    // app.delete("/api/notes/:id", function(req, res) {
+    //   let noteID = req.params.id;
+
+    //   const newNotes = notes.filter(note => note.id != noteID);
+    //   fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(newNotes, null, 2), function(error) {
+    //     if (error) throw error;
+    //     console.log("Note deleted...");
+    //     res.end()
+    //   })
     // })
+
+    
   });
 };
