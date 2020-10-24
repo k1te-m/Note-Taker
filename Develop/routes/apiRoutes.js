0885a38fb7d2b5e23769b26de64629a98e02d2b0
k1te <kmiller343@gmail.com>
2020-10-24 13:01:48 -0500

    notes not displaying

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,17 @@
 var path = require("path");
 var fs = require("fs");
 
-
-
-
-
 module.exports = function (app) {
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, data) {
-        if (error) throw error;
-        let notes = JSON.parse(data);
-    
-    app.get("/api/notes", function(req, res) {
-        res.json(notes);
-    })
+  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (
+    error,
+    data
+  ) {
+    if (error) throw error;
+    let notes = JSON.parse(data);
+
+    app.get("/api/notes", function (req, res) {
+      res.json(notes);
+    });
     app.post("/api/notes", function (req, res) {
       const noteAdded = req.body;
       const noteID = notes.length + 1;
@@ -23,9 +22,13 @@ module.exports = function (app) {
       };
       notes.push(newNote);
       console.log(notes);
-      fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), function(error) {
+      fs.writeFile(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify(notes, null, 2),
+        function (error) {
           if (error) throw error;
-      })
+        }
+      );
     });
     // app.post("/api/notes", function(req, res) {
     //     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(error, response) {
@@ -45,5 +48,5 @@ module.exports = function (app) {
     //         });
     //     });
     // })
-    
-})}
+  });
+};
