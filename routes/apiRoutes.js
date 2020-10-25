346abb927e9a2279c516aec65aeaf0d83fa814dd
k1te <kmiller343@gmail.com>
2020-10-25 16:02:42 -0500

    added get method for specific id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,10 +9,7 @@ module.exports = function (app) {
   ) {
     if (error) throw error;
     const notes = JSON.parse(data);
-    // Linked to the getNotes ajax function call in index.js, reads the currently saved notes objects in the notes array for display on the notes.html page
-    app.get("/api/notes", function (req, res) {
-      res.json(notes);
-    });
+
     // Post method for the creation of a new note, newNote object is created with an ID equal to it's future index position and title and text are assigned from req.body. newNote is then pushe dinto the notes array and the db.json file is rewritten with the new information
     app.post("/api/notes", function (req, res) {
       const noteAdded = req.body;
@@ -23,6 +20,7 @@ module.exports = function (app) {
         text: noteAdded.text,
       };
       notes.push(newNote);
+      console.log(notes);
       fs.writeFile(
         path.join(__dirname, "../db/db.json"),
         JSON.stringify(notes, null, 2),
@@ -33,6 +31,16 @@ module.exports = function (app) {
         }
       );
     });
+
+    // Linked to the getNotes ajax function call in index.js, reads the currently saved notes objects in the notes array for display on the notes.html page
+    app.get("/api/notes", function (req, res) {
+      res.json(notes);
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+      res.json(notes[req.params.id]);
+    })
+
     // Delete method tied to the deleteNote ajax function call in index.js. Utilizes the splice method to remove note from the array based on the req.params.id which is tied to the index position of the note within the notes array. Since noteID and the index position are equivalent the correct note will be removed. The one indicates we only want to remove one item from the array.
     app.delete("/api/notes/:id", function (req, res) {
       let noteID = req.params.id;
