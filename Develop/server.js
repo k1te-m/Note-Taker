067148cd8a580504d2fe22be2fb823b3f52be80c
k1te <kmiller343@gmail.com>
2020-10-25 13:45:04 -0500

    formatted

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -14,6 +14,6 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Set Server to Listen
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-})
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
