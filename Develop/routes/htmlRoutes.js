95a26415faf12f0f770e901ea3ae0e11749d4a52
k1te <kmiller343@gmail.com>
2020-10-25 13:45:18 -0500

    comments and formatting

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 var path = require("path");
 
-module.exports = function(app) {
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+// Routes for the notes and home pages, server serves up notes or index html file utilizing path based on URL
+module.exports = function (app) {
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-}
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
