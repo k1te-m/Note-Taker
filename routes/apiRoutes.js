f827394dfa7c980d25e933d7704a0f1c89ae1565
k1te <kmiller343@gmail.com>
2020-10-25 15:12:48 -0500

    rewrote delete method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,7 +23,6 @@ module.exports = function (app) {
         text: noteAdded.text,
       };
       notes.push(newNote);
-      console.log(notes);
       fs.writeFile(
         path.join(__dirname, "../db/db.json"),
         JSON.stringify(notes, null, 2),
@@ -31,15 +30,26 @@ module.exports = function (app) {
           if (error) throw error;
           res.redirect("/notes");
           console.log("Note created...");
+          console.log(notes);
         }
       );
     });
     // Delete method tied to the deleteNote ajax function call in index.js. Utilizes the splice method to remove note from the array based on the req.params.id which is tied to the index position of the note within the notes array. Since noteID and the index position are equivalent the correct note will be removed. The one indicates we only want to remove one item from the array.
     app.delete("/api/notes/:id", function (req, res) {
       let noteID = req.params.id;
-
-      notes.splice(noteID, 1);
-
+      
+      //For loop for the notes array that runs over the array and splices index item out if the id is equal to that of the id in the request parameters. 
+      for (let index = 0; index < notes.length; index++) {
+        if(notes[index].id == noteID) {
+          notes.splice(index, 1);
+        }
+      };
+      //For loop that resets the id values for the notes array items after an item has been removed to ensure id and index position continue to match
+      for (let index = 0; index < notes.length; index++) {
+        notes[index].id = index;
+      };
+      
+      console.log(notes);
       fs.writeFile(
         path.join(__dirname, "../db/db.json"),
         JSON.stringify(notes, null, 2),
@@ -49,6 +59,7 @@ module.exports = function (app) {
           res.end();
         }
       );
+      
     });
   });
 };
